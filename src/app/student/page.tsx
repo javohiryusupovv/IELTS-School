99ea7ce845647a7100e5cfedaa1c4bb7c085e8ae
Odilhon13 <yusupovjavoxir11@gmail.com>
2025-05-06 09:48:44 +0500

    fix scroll web app telegram

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -15,19 +15,19 @@ export default async function StudentDashboard() {
   const student = await getStudentFromCookie();  
   const coins = student?.coins?.reduce((sum: number, coin: any) => sum + coin.value, 0) ?? 0;
   return (
-    <div className="pt-5 min-h-screen pb-20">
+    <div className="pt-5 min-h-screen pb-20 container-cus">
       <article className="w-11/12 rounded-md m-auto mb-10">
         <div className=" relative top-0 left-0 rounded-[20px] overflow-hidden">
-          <Image className="absolute left-10 top-5 w-[150px]" src={IQLogo} alt="ChipCard"/>
-          <article className=" absolute top-16 w-full flex justify-between items-center px-10">
-            <Image src={CreditCardChip} alt="ChipCard"/>
-            <Image src={CreditCardWife} alt="CreditCard Wife" />
+          <Image className="absolute left-10 top-5 w-[150px] container-img" src={IQLogo} alt="ChipCard"/>
+          <article className="custom-cardBG absolute top-16 w-full flex justify-between items-center px-10">
+            <Image className="chip" src={CreditCardChip} alt="ChipCard"/>
+            <Image className="wife" src={CreditCardWife} alt="CreditCard Wife" />
           </article>
           <Image src={CreditCard} alt="CardCredit" />
-          <div className="flex justify-between w-full absolute bottom-0 h-[70px] px-10 backdrop-blur-lg bg-white/30">
+          <div className="custom-hold flex justify-between w-full absolute bottom-0 h-[70px] px-10 backdrop-blur-lg bg-white/30">
             <article className="mt-3"> 
-              <p className="text-[13px] text-white">Karta egasi</p>
-              <span className="text-[17px] font-medium text-white">{student?.surname} {student?.name}</span>
+              <p className="text-[13px] text-white holder-title">Karta egasi</p>
+              <span className="text-[17px] font-medium text-white holder-name">{student?.surname} {student?.name}</span>
             </article>
             <div className="mt-3">
               <p className="text-[13px] text-white mb-1">Coinlar</p>
