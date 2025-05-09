9dfd086d0aa55b437aac3bc9ea189d67c33918af
Odilhon13 <yusupovjavoxir11@gmail.com>
2025-05-09 12:19:09 +0500

    fix student tgbot, jarima, spend, poages filter

---
+++ src/app/student/history/jarima/page.tsx
@@ -0,0 +1,65 @@
+import { getStudentFromCookie } from "@/actions/student.check";
+import { formatDate, formatReasonText } from "../../../../../constants/page";
+import { Cog } from "lucide-react";
+
+export default async function CoinJarima() {
+  const students = await getStudentFromCookie();
+  const historyCoins = students?.coins?.map((coin: any) => {
+    return {
+      value: coin.value,
+      reason: coin.reasons.map((reason: any) => reason.reason),
+      date: coin.date,
+    };
+  });
+
+  const penaltyCoins = historyCoins?.filter(
+    (coin: any) =>
+      coin.value < 0 &&
+      !coin.reason.some((reason: string) =>
+        reason.includes("Coin almashtirildi !")
+      )
+  );
+
+  return (
+    <div className="w-11/12 m-auto pt-[75px]">
+      <article className="w-full">
+        <p className="text-[17px] font-semibold text-gray-400 mb-5">
+          Jarima Coinlar
+        </p>
+        {penaltyCoins && penaltyCoins.length > 0 ? (
+          <ul>
+            {penaltyCoins.map((coin: any, index: number) => (
+              <li
+                key={index}
+                className="flex justify-between items-center border-b py-2"
+              >
+                <article className="flex items-center gap-2">
+                  {coin.reason.map((reason: any, index: number) => (
+                    <p key={index} className="text-[14px]">
+                      {formatReasonText(reason)}
+                    </p>
+                  ))}
+                </article>
+                <article className="flex flex-col gap-1 justify-start items-end">
+                  <p className="flex justify-center items-center w-12 h-5 rounded-3xl text-[12px] text-white bg-red-500 p-2">
+                    {coin.value}
+                  </p>
+                  <span className="text-[10px] font-normal text-gray-400">
+                    {formatDate(coin.date)}
+                  </span>
+                </article>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="flex justify-center items-center h-[600px] relative">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-2">
+              <Cog className="animate-spin stroke-gray-500" />
+              <span className="text-gray-500">Jarimalar topilmadi !</span>
+            </div>
+          </div>
+        )}
+      </article>
+    </div>
+  );
+}
