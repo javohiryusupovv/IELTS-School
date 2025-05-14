9d0299b1f8307a0affc1fa1f4eebb2130a11caf4
Odilhon13 <yusupovjavoxir11@gmail.com>
2025-05-14 12:05:22 +0500

    fix leaderboard loading, status fix

--- src/app/student/leaderboard/_components/isLoading.tsx
+++ src/app/student/leaderboard/_components/isLoading.tsx
@@ -1,18 +1,27 @@
 import { Skeleton } from "@/components/ui/skeleton";
-
 import uuid4 from "uuid4";
+
 
 export default function ISLoading() {
   return (
     <div>
-      <div className="w-full flex justify-center relative top-14 gap-4">
-        {Array.from({ length: 3 }).map((_) => (
-          <div key={uuid4()}>
-            <Skeleton className="w-[130px] h-[150px] bg-red-500 rounded-none rounded-t-md">
-              <Skeleton className=" absolute z-[1] -top-10 left-6 w-20 h-20 rounded-full bg-yellow-50"></Skeleton>
+      <div className="w-full h-[230px] mb-4 flex justify-between relative top-20 gap-4">
+            <Skeleton className="w-[130px] h-[150px] bg-[#ffffff83] rounded-none rounded-t-3xl">
+              <Skeleton className=" absolute z-[1] -top-10 left-6 w-20 h-20 rounded-full bg-[#ffffff83]"></Skeleton>
             </Skeleton>
-          </div>
-        ))}
+            <Skeleton className="relative -top-6 w-[130px] h-[150px] bg-[#ffffff83] rounded-none rounded-t-3xl">
+              <Skeleton className="absolute z-[1] -top-12 left-[20%] w-20 h-20 rounded-full bg-[#ffffff83]"></Skeleton>
+            </Skeleton>
+            <Skeleton className="w-[130px] h-[150px] bg-[#ffffff83] rounded-none rounded-t-3xl">
+              <Skeleton className=" absolute z-[1] -top-10 right-6 w-20 h-20 rounded-full bg-[#ffffff83]"></Skeleton>
+            </Skeleton>
+      </div>
+      <div className="w-full flex flex-col gap-3">
+          {Array.from({length: 5}).map((_, ) => (
+                  <div key={uuid4()}>
+                    <Skeleton className="w-full h-[55px] bg-[#ffffff83]" />
+                  </div>
+          ))}
       </div>
     </div>
   );
