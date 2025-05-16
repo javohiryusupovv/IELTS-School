9fb0c1aa935127b420111860ab17274b422006a8
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2025-05-16 19:05:22 +0500

    get payment

--- src/app/crm/_components/TableTransactions.tsx
+++ src/app/crm/_components/TableTransactions.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { CheckCircle, Clock } from "lucide-react";
 import { data } from "./_constants";
+import { getPayments } from "@/actions/crmaccount.action";
+import { IPaymentAdd } from "../../../../app.types";
+import ImageCoin from "../../../../public/icons/roleTeacher.png"
 import Image from "next/image";
-import Link from "next/link";
 
-export default function TableTransactions() {
+
+export default async function TableTransactions() {
+    const product = await getPayments();
+
   return (
     <div className="overflow-x-auto rounded-xl bg-white p-6 shadow mb-9">
       <table className="min-w-full table-auto text-left text-sm">
@@ -12,53 +17,49 @@ export default function TableTransactions() {
           <tr className="text-gray-500">
             <th></th>
             <th className="py-3">Name</th>
-            <th className="py-3">Card</th>
             <th className="py-3">Assigned to</th>
             <th className="py-3">Last Transaction</th>
             <th className="py-3">Status</th>
-            <th className="py-3">End Date</th>
-            <th className="py-3">Total Used</th>
+            <th className="py-3">To'lov turi</th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200">
-          {data.map((item, id) => (
+          {product.map((item: IPaymentAdd, id:number) => (
             <tr key={id} className="hover:bg-gray-50">
               <td className="p-4">
                 <input type="checkbox" className="form-checkbox" />
               </td>
               <td className="flex items-center gap-3 py-4">
-                {/* <Image src={item.icon} alt={item.name} width={24} height={24} className="w-6 h-6"/> */}
-                <img src={item.icon} alt={item.icon} className="w-6 h-6" />
-                <span className="font-medium text-gray-900">{item.name}</span>
+                <Image src={ImageCoin} alt={item.markazTitle} width={24} height={24} className="w-6 h-6"/>
+                <span className="font-medium text-gray-900">{item.markazTitle}</span>
               </td>
-              <td className="text-gray-700">{item.card}</td>
               <td>
-                <span className="text-gray-700">{item.assigned.name}</span>
+                <span className="text-gray-700">{item.managerName}</span>
               </td>
               <td>
                 <div className="flex flex-col text-gray-700">
-                  <span>{item.date}</span>
-                  <span className="text-xs">{item.amount}</span>
+                  <span>{item.lastPayment}</span>
                 </div>
               </td>
               <td>
                 <span
                   className={`inline-flex items-center gap-1 rounded-full px-2 py-1 text-xs font-semibold ${
-                    item.status === "Done"
+                    item.cashStatus === "success"
                       ? "bg-green-100 text-green-700"
                       : "bg-orange-100 text-orange-700"
                   }`}
                 >
-                  {item.status === "Done" ? (
+                  {item.cashStatus === "success" ? (
                     <CheckCircle className="h-4 w-4" />
                   ) : (
                     <Clock className="h-4 w-4" />
                   )}
-                  {item.status}
+                  {item.cashStatus}
                 </span>
               </td>
-              <td className="text-gray-700">{item.endDate}</td>
-              <td className="font-semibold text-gray-800">{item.amount}</td>
+              <td>
+                <span className="text-gray-700">{item.cashType}</span>
+              </td>
             </tr>
           ))}
         </tbody>
