c9ef46d80951e3aca6ea1c96ecccc641785b934b
Odilhon13 <yusupovjavoxir11@gmail.com>
2025-05-21 14:05:28 +0500

    fix new env 55

--- src/app/student/leaderboard/page.tsx
+++ src/app/student/leaderboard/page.tsx
@@ -19,6 +19,7 @@ interface StudentType {
   total: number;
   coins?: CoinType[];
 }
+export const dynamic = "force-dynamic";
 
 export default function Leaderboard() {
   const [leaderUser, setLeaderUser] = useState<StudentType[]>([]);
