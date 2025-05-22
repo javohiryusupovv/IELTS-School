fc434d679de3f1366c5e4221d1265aca2340c33b
Javohir <yusupovjavoxir11@gmail.com>
2025-05-22 23:46:05 +0500

    fix unscalble fix3 3 2 5 3

--- src/app/teacher/layout.tsx
+++ src/app/teacher/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import NavbarTeacher from "./_components/navbar"
 
 export default async function Teacher({children}: {children: React.ReactNode}) {
@@ -5,6 +6,12 @@ export default async function Teacher({children}: {children: React.ReactNode}) {
 
   return (
     <div className="flex bg-white h-screen">
+       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
         <NavbarTeacher/>
         <div className="flex-grow sm:ml-[180px] ml-3 mr-[10px] mt-4 w-full mb-10">
             <main>{children}</main>
